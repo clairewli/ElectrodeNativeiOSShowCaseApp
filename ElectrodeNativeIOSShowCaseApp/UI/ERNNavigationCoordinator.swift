e078e20917435bbea8c8bb2b9eafc5d73d11db0b
w0l00qx <wli@walmartlabs.com>
2018-03-05 16:18:33 -0800

    Add ERNNavigationCoordinator

---
+++ ElectrodeNativeIOSShowCaseApp/UI/ERNNavigationCoordinator.swift
@@ -0,0 +1,13 @@
+//
+//  ERNNavigationCoordinator.swift
+//  ElectrodeNativeIOSShowCaseApp
+//
+//  Created by Claire Weijie Li on 3/5/18.
+//  Copyright © 2018 WalmartLabs. All rights reserved.
+//
+
+import UIKit
+
+class ERNNavigationCoordinator: NSObject {
+
+}
